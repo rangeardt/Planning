584bcf5390d08f3365ad0c47309cfd79194e39f9
Benoit Poulain <benoit.poulain@etu.univ-orleans.fr>
2014-03-18 08:50:08 +0100

    Ajout de la Base de données

---
+++ planning/src/WebAv/PlanningBundle/Entity/Activite.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace WebAv\PlanningBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Activite
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="WebAv\PlanningBundle\Entity\ActiviteRepository")
+ */
+class Activite
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nomactivite", type="string", length=255)
+     */
+    private $nomactivite;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nomactivite
+     *
+     * @param string $nomactivite
+     * @return Activite
+     */
+    public function setNomactivite($nomactivite)
+    {
+        $this->nomactivite = $nomactivite;
+
+        return $this;
+    }
+
+    /**
+     * Get nomactivite
+     *
+     * @return string 
+     */
+    public function getNomactivite()
+    {
+        return $this->nomactivite;
+    }
+}
---
+++ planning/src/WebAv/PlanningBundle/Entity/Date.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace WebAv\PlanningBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Date
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="WebAv\PlanningBundle\Entity\DateRepository")
+ */
+class Date
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
