e0c81f557b1af7419a9eb7bd78845399881d4892
Benoit Poulain <benoit.poulain@etu.univ-orleans.fr>
2014-03-18 17:32:43 +0100

    Creation d'une fonction de recherche dans la BDD

--- planning/src/WebAv/PlanningBundle/Controller/DefaultController.php
+++ planning/src/WebAv/PlanningBundle/Controller/DefaultController.php
@@ -12,18 +12,13 @@ class DefaultController extends Controller
     public function indexAction($year)
     {
 
-          $liste = $this->getDoctrine()
-                      ->getManager()
-                      ->getRepository('WebAvPlanningBundle:Reservation')
-                      ->findAll();
 
-
-    	 $ServiceDate = $this->container->get('webav_planning.date');
+    	$ServiceDate = $this->container->get('webav_planning.date');
 
    		$dates=$ServiceDate->getAll($year);
-		$datesJ=$ServiceDate->getAllJ($year);
-        $mois=$this->getRequest()->query->get('mois');
-        if( $mois == null ) $mois=date('n')-1;
+		  $datesJ=$ServiceDate->getAllJ($year);
+      $mois=$this->getRequest()->query->get('mois');
+      if( $mois == null ) $mois=date('n')-1;
         return $this->render('WebAvPlanningBundle:Default:index.html.twig', array(
         	'dates' => $dates ,
         	'datesJ' => $datesJ,
@@ -35,7 +30,15 @@ class DefaultController extends Controller
     }
 
     public function accueilAction(){
-        return $this->render('WebAvPlanningBundle:Default:accueil.html.twig',array());
+        $userManager = $this->container->get('fos_user.user_manager');
+        $user = $userManager->findUserByUsername($this->container->get('security.context')
+                      ->getToken()
+                      ->getUser());
+        $tab = $this->getDoctrine()
+                      ->getManager()
+                      ->getRepository('WebAvPlanningBundle:Reservation')
+                      ->getEventByMonth(2,2014,$user);
+        return $this->render('WebAvPlanningBundle:Default:accueil.html.twig',array('tab'=>$tab));
     }
 
 }
--- planning/src/WebAv/PlanningBundle/Entity/ReservationRepository.php
+++ planning/src/WebAv/PlanningBundle/Entity/ReservationRepository.php
@@ -3,7 +3,8 @@
 namespace WebAv\PlanningBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
-
+use \Datetime;
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
 /**
  * ReservationRepository
  *
@@ -12,4 +13,36 @@ use Doctrine\ORM\EntityRepository;
  */
 class ReservationRepository extends EntityRepository
 {
+	public function getEventByMonth($month,$year,$user)
+	{
+
+
+
+	
+	  $debut=new Datetime($year.'-'.($month+1).'-01');
+	  $fin=new Datetime($year.'-'.($month+2).'-01');
+
+	  $qb = $this->createQueryBuilder('Reserv')
+	  		->leftJoin('Reserv.usr', 'u')
+	  		->leftJoin('Reserv.date', 'd')
+	  		->leftJoin('Reserv.activite', 'a');
+
+	  $qb->where('u.id = :utilisateur')
+	  	 ->setParameter('utilisateur', $user);
+
+
+	  $qb->andWhere('d.date BETWEEN :debut AND :fin')
+      	 ->setParameter('debut', $debut)  // Date entre le 1er janvier de cette année
+      	 ->setParameter('fin', $fin); // Et le 31 décembre de cette année
+
+	  // On peut ajouter ce qu'on veut après
+	  $qb->orderBy('d.date', 'ASC');
+	  $tab=$qb->getQuery()->getResult();
+	  $mesreserv=array();
+	  foreach ($tab as $key => $r) {
+		$r['date']->getDate()	  	
+	  }
+	 return $tab;
+
+	}
 }
